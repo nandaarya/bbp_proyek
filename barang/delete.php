88a4091433684d665bd06fd073027f3c958934bc
Nanda Arya Putra <nandaarya404@gmail.com>
2022-11-02 21:16:45 +0700

    add fitur delete

---
+++ barang/delete.php
@@ -0,0 +1,13 @@
+<?php
+// include databse connection file
+include '../dbconnect.php';
+
+// Get id from URL to delete that user
+$idx = $_GET['idx'];
+
+// Delete user row from table based on gived id
+$result = mysqli_query($conn, "DELETE FROM barang WHERE idx=$idx");
+
+// After delete redirect to Home, so that latest list will be displayed.
+header("Location:index.php");
+?>
