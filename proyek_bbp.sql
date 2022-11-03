d7bfd1e5df9634b6f4778253856ebb12f39d376b
Nanda Arya Putra <nandaarya404@gmail.com>
2022-11-03 20:38:46 +0700

    add dumping data

--- proyek_bbp.sql
+++ proyek_bbp.sql
@@ -57,7 +57,23 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`idx`, `kode`, `nama`, `merek`, `jumlah`, `kondisi`, `fakultas`, `lokasi`, `pemakai`) VALUES
-(1, '3.05.02.01.003', 'Kursi Kuliah', 'Chitose', '73', 'Baik', 'Sains dan Teknologi', 'Ruang 304', 'Mahasiswa');
+(1, '3.05.02.04.004', 'A.C. Split', 'Panasonic', '5', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Ruang 104', 'Mahasiswa'),
+(2, '3.05.02.04.006', 'Kipas Angin', 'Panasonic F-EQ405', '2', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Ruang 104', 'Mahasiswa'),
+(3, '3.05.02.01.003', 'Kursi Kuliah', 'Cakra', '60', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Ruang 104', 'Mahasiswa'),
+(4, '3.05.01.05.048', 'LCD Projector/Infocus', 'Epson EB-X450', '1', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Ruang 104', 'Mahasiswa'),
+(5, '3.05.01.05.010', 'White Board', 'Kaca Hand Made', '1', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Ruang 104', 'Mahasiswa'),
+(6, '3.05.01.05.002', 'Hidran', ' ', '2', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Lobby Lantai 3', 'Mahasiswa'),
+(7, '3.05.02.01.003', 'Kursi Besi', 'Panjang', '3', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Lobby Lantai 3', 'Mahasiswa'),
+(8, '3.05.01.05.008', 'Papan Visual', 'P. Pengumuman', '1', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Lobby Lantai 3', 'Mahasiswa'),
+(9, '3.05.01.05.001', 'Tabung Pamadam', 'Appron AP-35P', '2', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Lobby Lantai 3', 'Mahasiswa'),
+(10, '3.05.02.04.004', 'A.C. Split', 'Gree (2 PK)', '1', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Ruang 304', 'Mahasiswa'),
+(11, '3.05.02.04.006', 'Kipas Anging', 'Panasonic F-EQ405', '2', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Ruang 304', 'Mahasiswa'),
+(12, '3.05.02.01.003', 'Kursi Dosen', 'Stramm', '1', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Ruang 304', 'Dosen'),
+(13, '3.05.02.01.003', 'Kursi Kuliah', 'Chitose', '73', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Ruang 304', 'Mahasiswa'),
+(14, '3.05.01.05.048', 'LCD Projector/Infocus', 'Epson EB-W28', '1', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Ruang 304', 'Mahasiswa'),
+(15, '3.05.02.01.002', 'Meja Kerja Kayu', 'Pronov', '1', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Ruang 304', 'Dosen'),
+(16, '3.05.01.05.010', 'White Board', 'Kaca Hand Made', '1', 'Baik', 'FAKULTAS SAINS DAN TEKNOLOGI', 'Ruang 304', 'Dosen')
+;
 
 -- --------------------------------------------------------
 
